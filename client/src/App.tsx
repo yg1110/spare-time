b766a1a14ff2e74d925ebb53c43f86f9778e981d
yg1110 <younggil9488@gmail.com>
2023-04-06 00:17:41 +0900

    styled-components / material-ui init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
 function App() {
-  return <div className="App"></div>
+  return <Button variant="text">Text</Button>
 }
 
 export default App
